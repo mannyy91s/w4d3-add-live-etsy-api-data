8e91d9a816b46194604b5ff368d935ec6685ed22
Manpreet Singh <mannyy91s@gmail.com>
2016-10-19 15:48:34 -0400

    added fetch data

--- js/global.js
+++ js/global.js
@@ -1,3 +1,9 @@
+fetch('http://thinksaydo.com/tiyproxy.php?url=' + encodeURIComponent('https://openapi.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=' + encodeURIComponent('board games') + '&includes=Images,Shop'))
+.then(response => response.json())
+.then(response => response.results.forEach(function(item){
+    makeThemBoxes(item)
+}))
+
 function makeThemBoxes(item){
     var card = document.createElement('div')
     card.className ='col-sm-3 card'
@@ -26,101 +32,3 @@ function makeThemBoxes(item){
 
     document.querySelector('#searchResults ').appendChild(card)
 }
-
-var items = [
-    {
-        image: item.url,
-        title: item.title,
-        seller: item.who_made,
-        price: item.price,
-    },
-    {
-        image: item.url,
-        title: item.title,
-        seller: item.who_made,
-        price: item.price,
-    }
-]
-
-items.forEach(function(item){
-
-})
-
-var items = [
-    {
-        image: 'http://unsplash.it/200?image=23',
-        title: 'Vintage Board Game',
-        seller: 'franz66',
-        price: '$'+ 15.00
-    },
-    {
-        image: 'http://unsplash.it/200?image=24',
-        title: 'Vintage Game of Life',
-        seller: 'vintagetoolbox',
-        price:'$'+  20.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'PaloAltoOpoly Board Game. Monoply Variant',
-        seller: 'SecondSymphony',
-        price:'$'+  150.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'Vintage 1960s split-level',
-        seller: 'BuyfromGroovy',
-        price:'$'+  13.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'retro chess chess pieces chess set',
-        seller: 'Cleopatravintage',
-        price:'$'+  8.48
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'Vintage Inspector Gadget Board Game',
-        seller: 'FunkyKoala',
-        price:'$'+ 20.89
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'vtg Go Board -- Reiss Game',
-        seller: 'Vintagebyjessie',
-        price:'$'+  12.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'Parcheesi By Selchow & Righter Co. 1959',
-        seller: 'BigYellowRetro',
-        price:'$'+  15.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: '1955 Candy Land, Milton Bradley vintage',
-        seller: 'AhiddenNest',
-        price:'$'+  54.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'Vintage Emperors Challenge Educational',
-        seller: 'WylieOwlVintage',
-        price:'$'+  12.00
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: 'Rules of Mille Bornes 1960, vintage game',
-        seller: 'RecreVintage',
-        price:'$'+  7.90
-    },
-    {
-        image: 'http://unsplash.it/200?random',
-        title: '1985 Complete Wheel of Fortune',
-        seller: 'StutzyVintage',
-        price:'$'+  9.99
-    },
-]
-
-results.forEach(function(result) {
-    makeThemBoxes(item)
-})
